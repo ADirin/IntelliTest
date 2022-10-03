960eb0687a8ed760bb08d24dc10b740d7ad7c4f0
Amir Dirin <amir.dirin@yahoo.com>
2022-10-03 18:40:38 +0300

    Add additional test

--- TestMe.java
+++ TestMe.java
@@ -2,5 +2,11 @@ public class TestMe {
     public static void main(String[] args) {
         System.out.println("Jenkins Test");
         System.out.println("lets check");
+
+        System.out.println(printMe());
+    }
+    public static String printMe() {
+        return "call me";
+
     }
 }
